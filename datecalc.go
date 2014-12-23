4a258099e4d3f51779d3713d130b5a8cc0390e4c
Vi <development@pariahvi.com>
2014-12-23 05:54:17 -0600

    1.0.3.4
    Updated typo, added BSD 2-Clause License, and fixed a problem where the program thought January was February.

--- datecalc.go
+++ datecalc.go
@@ -1,10 +1,32 @@
-// Copyright 2012-2014 PariahVi (http://pariahvi.com).
-// LatchBox is licensed under a BSD License.
-// Read LICENSE.txt for more license text.
+/* Copyright (C) 2014, PariahVi
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *     1. Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *     2. Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY AUTHOR AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+ 
+/* A Library to Calculate the Day of the Week of Any Date */
 
-// A Library to Calculate the Day of the Week of Any Date
-
-// Version 1.0.3.3
+/* Version 1.0.3.4 */
 
 package datecalc
 
@@ -84,7 +106,7 @@ func isLeapYear(year int, calType string) bool {
     return false
 }
 
-// Check if the date (year, month, date) exists if cal_type.
+// Check if the date (year, month, date) exists in cal_type.
 func isRealDate(year int, month, date int8, calType string) bool {
     month30 := []int8{4, 6, 9, 11}
     if inRange(month, 1, 13) == false {
@@ -115,7 +137,7 @@ func isRealDate(year int, month, date int8, calType string) bool {
         }
         if inMonths && date > 30 {
             return false
-        } else if month == 1 && !(date < 29 || (isLeapYear(year, calType) &&
+        } else if month == 2 && !(date < 29 || (isLeapYear(year, calType) &&
                 date == 29)) {
             return false
         }
